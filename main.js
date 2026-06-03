6a69f0b73886a74a6139a82cd155632ff0e64c3f
michaelmarajda-create <michaelmarajda@gmail.com>
2026-06-03 23:52:00 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+for (var i = 1; i <= 20; i++){
+    if (i%2 !== 0){
+        console.log (i);
+    }
+}
+
+/////////
+
+for (var i= 100; i>=3; i--){
+    if (i%3 === 0){
+        console.log (i);
+    }
+}
+
+/////////
+
+var Psequence = [4, 2.5, 1, -0.5, -2, -3.5];
+
+for (var i = 0; i< Psequence.length; i++){
+    console.log ([i]);
+}
